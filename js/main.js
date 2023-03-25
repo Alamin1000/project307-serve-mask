17cfdc3096850fa6dded991816fc9d6e3f7fc7d6
alamin <alaminhosen44000@gmail.com>
2023-03-26 00:52:46 +0600

    complete

---
+++ js/main.js
@@ -0,0 +1,63 @@
+(function ($) {
+  "use strict";
+
+  // offcanvas-js
+  $(".offcanvas-open").click(function () {
+    $(".offcanvas-menu").addClass("active");
+    $(".offcanvas-overlay").addClass("active");
+  });
+  $(".offcanvas-menu a").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(".close-offcanvas").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(document).mouseup(function (e) {
+    var container = $(".offmenu");
+
+    // If the target of the click isn't the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $(".offcanvas-menu").removeClass("active");
+      $(".offcanvas-overlay").removeClass("active");
+    }
+  });
+
+  // owl-carousel
+  $(".testimonial-slider-active").owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 70,
+    responsiveClass: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="far fa-arrow-left"></i>',
+      '<i class="far fa-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        margin: 30,
+        nav: false,
+        dots: true,
+      },
+      991: {
+        margin: 30,
+      },
+      1199: {},
+      1499: {},
+    },
+  });
+})(jQuery);
+
+$(document).ready(function () {
+  // scroll up
+  $(function () {
+    $.scrollUp();
+  });
+
+  // preloader
+  $("#preloader").fadeOut(500);
+});
